be4d42ff3467c97b52df46265120f963090daf52
Sunil Reddy Boreddy <sunilboreddy@gmail.com>
2021-01-06 15:27:23 +0000

    Updated again

--- models/staging/jaffle_shop/stg_customers.sql
+++ models/staging/jaffle_shop/stg_customers.sql
@@ -1,9 +1,6 @@
-with customers as (
-    
-    select 
-        id as customer_id,
-        first_name,
-        last_name
+select 
+    id as customer_id,
+    first_name,
+    last_name
 
-    from {{source('jaffle_shop','customers')}}
-)
+from {{source('jaffle_shop','customers')}}
