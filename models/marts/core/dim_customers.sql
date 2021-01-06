cb85a978233edf53f292e7f6f0595e3da2969d67
sunilboreddy <sunilboreddy@gmail.com>
2021-01-06 11:13:17 +0000

    Dim_customers changes

--- models/marts/core/dim_customers.sql
+++ models/marts/core/dim_customers.sql
@@ -6,7 +6,7 @@ with customers as (
 
 orders as (
 
-     select * from {{ ref('stg_orders') }}
+     select * from {{ ref('fct_orders') }}
     
 ),
 
@@ -14,7 +14,6 @@ customer_orders as (
 
     select
         customer_id,
-
         min(order_date) as first_order_date,
         max(order_date) as most_recent_order_date,
         count(order_id) as number_of_orders,
